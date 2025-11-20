1a3f5a4391dec43b97185826b028a705fee25409
Jan284573 <284573@student.pwr.edu.pl>
2025-11-20 12:01:48 +0100

    L2P2

--- src/L2Form.java
+++ src/L2Form.java
@@ -1,4 +1,6 @@
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,6 +10,12 @@ public class L2Form {
     private JTextField UsernameTextField;
     private JTextArea outputTextArea;
     private JPasswordField passwordField1;
+    private JButton moveLeftButton;
+    private JButton moveRightButton;
+    private JButton scaleChangeButton;
+    private JPanel movePanel;
+    private JLabel TestObject;
+    private JButton colorButton;
 
 
     public L2Form() {
@@ -17,12 +25,83 @@ public class L2Form {
                 String name = UsernameTextField.getText();
                 String password = new String(passwordField1.getPassword());
 
-                outputTextArea.append("Name: "+name+"\n");
-                outputTextArea.append("Password: "+password+"\n");
+                if(outputTextArea.getText().length()>300){
+                    outputTextArea.setText("");
+                }
+
+                outputTextArea.append("\nName: "+name+"\n");
+                outputTextArea.append("Password: "+password);
 
                 UsernameTextField.setText("Username");
                 passwordField1.setText("");
 
+
+
+            }
+        });
+
+        movePanel.setLayout(null);
+        TestObject.setOpaque(true);
+        if(TestObject.getWidth() ==0 || TestObject.getHeight() ==0){
+            TestObject.setBounds(100,20,100,25);
+        }
+
+
+        scaleChangeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(TestObject.getWidth()==100 && TestObject.getHeight()==25) {
+                    TestObject.setSize(200, 50);
+                    TestObject.setText("200");
+                } else {
+                    TestObject.setSize(100, 25);
+                    TestObject.setText("100");
+                }
+                TestObject.revalidate();
+                TestObject.repaint();
+            }
+        });
+
+
+        moveLeftButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int x = TestObject.getX();
+                int y = TestObject.getY();
+                if (x > 0) {
+                    TestObject.setLocation((x - 50), y);
+                }
+                //outputTextArea.append("\n"+(x-50));
+                TestObject.repaint();
+            }
+        });
+
+        moveRightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int x = TestObject.getX();
+                int y = TestObject.getY();
+                if (x+50 < movePanel.getWidth()-TestObject.getWidth()) {
+                    TestObject.setLocation((x + 50), y);
+                }
+                //outputTextArea.append("\n"+(x+50));
+                TestObject.repaint();
+            }
+        });
+
+
+        colorButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Color actual = TestObject.getBackground();
+                Color chosen = JColorChooser.showDialog(
+                        TestObject,
+                        "Wybierz kolor tła",
+                        actual
+                );
+                if (chosen != null) {
+                    TestObject.setBackground(chosen);
+                }
             }
         });
     }
